4beeb5ac90114226528e3e805b49acdcd91e7916
Patrick Griffin <58729+firien@users.noreply.github.com>
2022-01-01 21:36:58 -0500

    idiot fix

--- src/png.js
+++ src/png.js
@@ -2,8 +2,7 @@
 export default (buffer) => {
   // verify PNG header
   // https://en.wikipedia.org/wiki/Portable_Network_Graphics#File_header
-  // 0x89 0x50 0x4E 0x47 0x0D 0x0A 0x1A 0x0A
-  if (buffer.readUInt32BE(0) === 2303741511 && buffer.readUInt32BE(4) === 218765834) {
+  if (buffer.readUInt32BE(0) === 0x89504E47 && buffer.readUInt32BE(4) === 0x0D0A1A0A) {
     // assume IHDR starts at offset 16
     return {
       width: buffer.readUInt32BE(16),
